3a1fb9ba92f97d362dbcb4558667a7d322418a9a
Ivan Gonzalez <ivan.gon.ro@hotmail.com>
2022-11-13 18:19:33 -0500

    definindo caracteristicas de una celula

---
+++ cell_behavior.py
@@ -0,0 +1,35 @@
+"""
+Organelos
+Los organelos son estructuras que llevan a cabo diferentes funciones dentro de la célula.
+Los organelos en una célula son análogos a los órganos del cuerpo.
+Los organelos están suspendidos en un fluido a base de agua llamado citosol.
+
+Núcleo
+
+El núcleo guarda toda la información genética (cromosomas) de una célula eucariota.
+El núcleo es esférico y está rodeado por dos membranas.
+El núcleo es el "cerebro" de la célula.
+
+Mitocondria
+
+La mitocondria sirve como fuente de energía para la célula.
+La mitocondria convierte biomoléculas (como grasas y azúcar) en energía.
+Los sub-productos de la producción de energía en la mitocondria pueden dañar al ADN y causar mutaciones.
+
+Ribosomas
+
+Los ribosomas están hechos de 2 complejos grandes de ARN y proteína.
+Los ribosomas se encuentran en el citosol. Su función es leer el ARN y producir proteínas. A este proceso se le llama traducción.
+
+Citoesqueleto
+
+El citoesqueleto es una red extensa de proteínas que entrecruzan el citoplasma de una célula.
+La actina y la tubilina son las proteínas empleadas para crear las fibras principales del citoesqueleto (microfilamentos y microtubulos, respectivamente).
+El citoesqueleto tiene varias fuciones clave:
+Da estructura a la célula y un lugar donde anclar a los organelos
+Movilidad a la célula
+Control de la división celular durante la mitosis.
+Cambios en el citoesqueleto que permiten un incremento en el movimiento de la célula son observados en células con cáncer.
+Muchas de las medicinas anti-cáncer trabajan al interferir con la actividad de proteínas del citoesqueleto.
+"""
+
